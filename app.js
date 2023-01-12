ac5a5947e4ad2b0d9d4a05574cf91402e8c5089b
jonw93 <jonwarnekgultom@gmail.com>
2023-01-12 23:07:58 +0700

    commit I

---
+++ app.js
@@ -0,0 +1,12 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElments = document.querySelectorAll(".hidden");
+hiddenElments.forEach((el) => observer.observe(el));
